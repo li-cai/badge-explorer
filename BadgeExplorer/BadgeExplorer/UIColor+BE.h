615a5dc130c922f2e7ee92d9f7d4598a760b9221
Cailin Li <cxl2467@rit.edu>
2015-03-04 21:44:13 -0500

    Add category to extend UIColor

---
+++ BadgeExplorer/BadgeExplorer/UIColor+BE.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+BE.h
+//  BadgeExplorer
+//
+//  Created by Cailin Li on 3/4/15.
+//  Copyright (c) 2015 Cailin Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (BE)
+
++ (UIColor *)leafColor;
+
+@end
---
+++ BadgeExplorer/BadgeExplorer/UIColor+BE.m
@@ -0,0 +1,24 @@
+//
+//  UIColor+BE.m
+//  BadgeExplorer
+//
+//  Created by Cailin Li on 3/4/15.
+//  Copyright (c) 2015 Cailin Li. All rights reserved.
+//
+
+#import "UIColor+BE.h"
+
+@implementation UIColor (BE)
+
++ (UIColor *)leafColor {
+    return [UIColor colorWithHex:0x96ab4f];
+}
+
++ (UIColor *)colorWithHex:(unsigned)rgbValue {
+    return [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+                           green:((float)((rgbValue & 0x00FF00) >>  8))/255.0 \
+                            blue:((float)((rgbValue & 0x0000FF) >>  0))/255.0 \
+                           alpha:1.0];
+}
+
+@end
